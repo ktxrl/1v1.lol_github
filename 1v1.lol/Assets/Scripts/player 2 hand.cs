12102e9966a6590fa1b9f58dfb85e69acdf7b6c0
Shotobob <109097353+Shotobob@users.noreply.github.com>
2026-03-24 11:50:11 -0500

    b

---
+++ 1v1.lol/Assets/Scripts/player 2 hand.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player2hand : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(mousePos.x, mousePos.y);
+    }
+}
