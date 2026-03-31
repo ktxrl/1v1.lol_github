099a604c0e88859d6f2786e9c3c106bae427efc5
ktxrl <linryan1016@gmail.com>
2026-03-31 11:51:22 -0500

    afasdfs

--- 1v1.lol/Assets/Scripts/Enemy.cs
+++ 1v1.lol/Assets/Scripts/Enemy.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject manager;
+    [SerializeField] float speed;
+    [SerializeField] int enemy; //0 = flying, 1 = skeleton, 2 = lizard
+    public bool controlling, direction;
+    Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        controlling = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (controlling)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                rb.linearVelocity = new Vector2(-speed, rb.linearVelocityY);
+            } 
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                rb.linearVelocity = new Vector2(speed, rb.linearVelocityY);
+            }
+            else
+            {
+                rb.linearVelocity = new Vector2(0, rb.linearVelocityY);
+            }
+        }
+        else
+        {
+
+        }
+    }
+    public void OnMouseDown()
+    {
+        controlling = true;
+        if (enemy == 0) manager.GetComponent<TullyMonster67>().FlyingSelect();
+        else if (enemy == 1) manager.GetComponent<TullyMonster67>().SkeletonSelect();
+        else manager.GetComponent<TullyMonster67>().LizardSelect();
+    }
+    public void Deselect()
+    {
+        controlling = false;
     }
 }
--- 1v1.lol/Assets/Scripts/TullyMonster67.cs
+++ 1v1.lol/Assets/Scripts/TullyMonster67.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class TullyMonster67 : MonoBehaviour
 {
-    //[SerializeField] 
+    [SerializeField] Enemy flying;
+    [SerializeField] Enemy skeleton;
+    [SerializeField] Enemy lizard;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +16,23 @@ public class TullyMonster67 : MonoBehaviour
     void Update()
     {
         
+    }
+    public void FlyingSelect()
+    {
+
+        skeleton.GetComponent<Enemy>().Deselect();
+        lizard.GetComponent<Enemy>().Deselect();
+    }
+    public void SkeletonSelect()
+    {
+
+        flying.GetComponent<Enemy>().Deselect();
+        lizard.GetComponent<Enemy>().Deselect();
+    }
+    public void LizardSelect()
+    {
+
+        flying.GetComponent<Enemy>().Deselect();
+        skeleton.GetComponent<Enemy>().Deselect();
     }
 }
